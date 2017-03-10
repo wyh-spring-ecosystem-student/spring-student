9a1010576c3918c060798fdf9f25c03c0797a9b5
yuhao.wang@msxf.com <xiaolyuh@163.com>
2017-03-10 17:53:38 +0800

    修改忽略配置

--- spring-student/src/main/java/com/chenfeng/xiaolyuh/config/AopConfig.java
+++ spring-student/src/main/java/com/chenfeng/xiaolyuh/config/AopConfig.java
@@ -1,13 +1,14 @@
 package com.chenfeng.xiaolyuh.config;
 
-import static org.hamcrest.CoreMatchers.not;
-
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.FilterType;
+
+import com.chenfeng.xiaolyuh.entity.InitBeanAndDestroyBean;
 
 @Configuration // 声明当前类是一个配置类，相当于Spring配置的XML文件
-@ComponentScan(value="com.chenfeng.xiaolyuh", excludeFilters=not()) // 扫描包
+@ComponentScan(basePackages={"com.chenfeng.xiaolyuh"}, excludeFilters={@ComponentScan.Filter(type=FilterType.ASSIGNABLE_TYPE, value=InitBeanAndDestroyBean.class)}) // 扫描包
 @EnableAspectJAutoProxy // 使用@EnableAspectJAutoProxy注解开启Spring对AspectJ代理的支持
 public class AopConfig {
 	
