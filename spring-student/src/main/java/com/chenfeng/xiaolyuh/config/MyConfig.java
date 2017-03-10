606c1da1d3cc50765c977c007939700547639eac
yuhao.wang@msxf.com <xiaolyuh@163.com>
2017-03-10 14:32:16 +0800

    修改 配置文件

--- spring-student/src/main/java/com/chenfeng/xiaolyuh/config/MyConfig.java
+++ spring-student/src/main/java/com/chenfeng/xiaolyuh/config/MyConfig.java
@@ -10,11 +10,11 @@ import com.chenfeng.xiaolyuh.entity.InitBeanAndDestroyBean;
 /**
  * Created by yuhao.wang on 2017/3/9.
  */
-@Configuration // 声明成配置文件
+@Configuration // 声明当前类是一个配置类，相当于Spring配置的XML文件
 @ComponentScan("com.chenfeng.xiaolyuh") // 扫描包
 public class MyConfig {
 	
-	@Bean // 声明成一个begin，名字默认是方法名
+	@Bean // 注解在方法上，声明当前方法返回值是一个Bean。Bean的名称默认是方法名。
 	public DogEntity getDogEntity() {
 		return new DogEntity();
 	}
